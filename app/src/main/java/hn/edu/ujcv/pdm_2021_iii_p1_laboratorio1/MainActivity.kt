bda4c9b9338a20e556276193a077e1fc95744730
Hepburn <91041472+Hepburn23@users.noreply.github.com>
2021-10-01 21:52:57 -0600

    Añadiendo mainactivity con sus instancias, ENEE, SANAA, DebitarCuenta, AcreditarCuenta, Recargas de CLARO y TIGO

--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_laboratorio1/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p1_laboratorio1/MainActivity.kt
@@ -2,11 +2,53 @@ package hn.edu.ujcv.pdm_2021_iii_p1_laboratorio1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    object Main {
+        fun mostrarMenu() {
+            println("App")
+            println("1- ENEE")
+            println("2- SANAA")
+            println("3- RecargaCLARO")
+            println("4- RecargaTIGO")
+            println("5- DebitarCuenta")
+            println("6- AcreditarCuenta")
+            println("7- Salir")
+        }
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            //Objeto Scanner
+            val sc = Scanner(System.`in`)
+
+            //Variable
+            var respuestaUser = 0
+            mostrarMenu()
+            print("Su opcion: ")
+            respuestaUser = sc.nextInt()
+            respuestaUser = when (respuestaUser) {
+                1, 2, 3, 4, 5, 6, 7 -> {
+                    println("Escriba una opción correcta")
+                    println(" ")
+                    mostrarMenu()
+                    print("Su opcion: ")
+                    sc.nextInt()
+                }
+                else -> {
+                    println("Escriba una opción correcta")
+                    println(" ")
+                    mostrarMenu()
+                    print("Su opcion: ")
+                    sc.nextInt()
+                }
+            }
+        }
     }
 
 
